3e1467f017c4927784ce266fc17255d948734437
Tom McArdle <79710668+tmc-ee@users.noreply.github.com>
2026-06-15 19:17:40 +0100

    CDMS-BAU: Add Floci to Docker Compose

--- scripts/floci/99-ready.sh
+++ scripts/floci/99-ready.sh
@@ -2,7 +2,7 @@
 
 function is_ready() {
   # test-reports
-  aws --endpoint-url $LOCALSTACK_ENDPOINT s3api head-bucket --bucket reports || return 1
+  awslocal s3 ls s3://reports || return 1
 
   return 0
 }
@@ -11,5 +11,3 @@ while ! is_ready; do
   echo "Waiting until ready"
   sleep 1
 done
-
-echo READY > /tmp/READY
--- scripts/localstack/10-setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# test-reports
-aws --endpoint-url $LOCALSTACK_ENDPOINT s3api create-bucket --region $AWS_REGION --create-bucket-configuration LocationConstraint=$AWS_REGION --bucket reports
